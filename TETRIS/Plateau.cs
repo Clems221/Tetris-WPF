5b05136b7719a67ea5aa2ab1446f7eae1f0d0ed0
Clement Lusson <clementlusson@gmail.com>
2015-12-10 19:27:09 +0100

    Rotation(Vers droite) OK! Ajout Commentaires

--- TETRIS/Plateau.cs
+++ TETRIS/Plateau.cs
@@ -19,12 +19,16 @@ namespace TETRIS
         private Piece Piece;
         private Label[,] BlockControls;
         static private Brush Nocolor = Brushes.Transparent;
-        static private Brush GrayColor = Brushes.Gray;
+        static private Brush BlackColor = Brushes.Black;
         #region création de la grille de jeu
+        //Nous avons besoin : lignes, colonnes , score, lignes remplies et quelques effets
         public Plateau(Grid TetrisGrid)
         {
+            //État actuel des valeurs , les lignes et les colonnes sont à compter,
+            //elles ont déjà été déclarés dans le fichier XAML.
             Lignes = TetrisGrid.RowDefinitions.Count;
             Colonnes = TetrisGrid.ColumnDefinitions.Count;
+
             Score = 0;
             LignesRemplies = 0;
 
@@ -35,7 +39,7 @@ namespace TETRIS
                 {
                     BlockControls[i, j] = new Label();
                     BlockControls[i, j].Background = Nocolor;
-                    BlockControls[i, j].BorderBrush = GrayColor;
+                    BlockControls[i, j].BorderBrush = BlackColor;
                     BlockControls[i, j].BorderThickness = new Thickness(0.3, 0.3, 0.3, 0.3);
                     Grid.SetRow(BlockControls[i, j], j);
                     Grid.SetColumn(BlockControls[i, j], i);
@@ -56,7 +60,7 @@ namespace TETRIS
             return LignesRemplies;
         }
         #endregion
-        #region colorier les pièces
+        #region Représenter les pièces
         private void pieceDessin()
         {
             Point Position = Piece.getPosition();
@@ -70,6 +74,7 @@ namespace TETRIS
         }
         #endregion
         #region Supression de lignes
+
         private void currJeuSuppr()
         {
             Point Position = Piece.getPosition();
@@ -80,6 +85,7 @@ namespace TETRIS
                     (int)(S.Y + Position.Y) + 2].Background = Nocolor;
             }
         }
+        //Méthode pour contrôler les lignes
         private void CheckLignes()
         {
             bool rempli;
@@ -103,6 +109,7 @@ namespace TETRIS
                 }
             }
         }
+        //Méthode pour supprimer une ligne remplie
         private void SupprLigne(int row)
         {
             for (int i = row; i > 2; i--)
@@ -115,6 +122,7 @@ namespace TETRIS
         }
         #endregion
         #region Mouvements pièces
+        //Méthodes de vérification pour décaler les pièces vers Gauche/Droite
         public void PieceMovLeft()
         {
             Point Position = Piece.getPosition();
@@ -123,10 +131,12 @@ namespace TETRIS
             currJeuSuppr();
             foreach (Point S in Forme)
             {
+                //Si on touche le bord du plateau
                 if (((int)(S.X + Position.X) + ((Colonnes / 2) - 1) - 1) < 0)
                 {
                     move = false;
                 }
+                //Sinon si on touche une autre pièce
                 else if (BlockControls[((int)(S.X + Position.X) + ((Colonnes / 2) - 1) - 1),
                     (int)(S.Y + Position.Y) + 2].Background != Nocolor)
                 {
@@ -151,10 +161,12 @@ namespace TETRIS
             currJeuSuppr();
             foreach (Point S in Forme)
             {
+                //Si on touche le bord du plateau
                 if (((int)(S.X + Position.X) + ((Colonnes / 2) - 1) + 1) >= Colonnes)
                 {
                     move = false;
                 }
+                //Sinon si on touche une autre pièce
                 else if (BlockControls[((int)(S.X + Position.X) + ((Colonnes / 2) - 1) + 1),
                     (int)(S.Y + Position.Y) + 2].Background != Nocolor)
                 {
@@ -171,6 +183,7 @@ namespace TETRIS
                 pieceDessin();
             }
         }
+        //Méthode pour vérifier si le mouvement vers le bas est possible
         public void PieceMovDown()
         {
             Point Position = Piece.getPosition();
@@ -179,15 +192,17 @@ namespace TETRIS
             currJeuSuppr();
             foreach (Point S in Forme)
             {
+                //La pièce est en bas du plateau
                 if (((int)(S.Y + Position.Y) + 2 + 1) >= Lignes)
                 {
                     move = false;
                 }
+                //Si pièce en dessous
                 else if (BlockControls[((int)(S.X + Position.X) + ((Colonnes / 2) - 1)),
                     (int)(S.Y + Position.Y) + 2 + 1].Background != Nocolor)
                 {
                     move = false;
-                    // Ne marche pas !
+                    //Si la pièce touche une autre au niveau de la première ligne
                     if (S.X + Position.X == 0 && S.Y + Position.Y == -1)
                     {
                         partiePerdue();
@@ -206,6 +221,7 @@ namespace TETRIS
                 Piece = new Piece();
             }
         }
+        //Méthode de vérification pour tourner la pièce
         public void PieceRotation()
         {
             Point Position = Piece.getPosition();
@@ -219,25 +235,27 @@ namespace TETRIS
                 double x = S[i].X;
                 S[i].X = S[i].Y * -1;
                 S[i].Y = x;
+                //Si on touche le bas du plateau
                 if (((int)((S[i].Y + Position.Y) + 2)) >= Lignes)
                 {
                     move = false;
                 }
+                //Si la pièce dépasse du plateau sur la gauche
                 else if (((int)(S[i].X + Position.X) + ((Colonnes / 2) - 1)) < 0)
                 {
                     move = false;
                 }
-                //problème lors de la rotation au bord 
+                //Si la pièce dépasse du plateau sur la droite
+                else if (((int)(S[i].X + Position.X) + ((Colonnes / 2) - 1)) >= Colonnes)
+                {
+                    move = false;
+                }
+                //Si la pièce touche une autre pièce
                 else if (BlockControls[((int)(S[i].X + Position.X) + ((Colonnes / 2) - 1)),
                     (int)(S[i].Y + Position.Y) + 2].Background != Nocolor)
                 {
                     move = false;
                 }
-                else if (((int)(S[i].X + Position.X) + ((Colonnes / 2) - 1)) >= Lignes)
-                {
-                    move = false;
-                }
-
             }
             if (move)
             {
@@ -252,6 +270,7 @@ namespace TETRIS
         #endregion
         public void partiePerdue()
         {
+
             MessageBox.Show("Partie perdue ! \n\nVotre score : " + Score.ToString("000000") +
                             "\nNombre de lignes remplies : " + LignesRemplies.ToString("000000"));
             Application Tetris = Application.Current;
